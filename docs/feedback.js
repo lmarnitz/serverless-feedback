ef18d8ebec28d5051bf44d5fd4e7fb249512fdf3
Laura Marnitz <laura.marnitz@de.ibm.com>
2019-03-29 15:08:52 +0100

    change url

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/feedback';
+const apiUrl = 'https://6811f9b8.eu-de.apiconnect.appdomain.cloud/feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
